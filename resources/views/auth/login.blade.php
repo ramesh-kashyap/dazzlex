eef8233c0f0596a1f07678c588733b583d50649c
rajkumarkashyap3582 <rajkumarkashyap3582@gmail.com>
2024-10-14 12:24:22 +0530

    sdfgh

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,382 @@
+@include('layouts.mainsite.header')
+
+<main class="netmi-content"><div class="netmi-main">
+     <div class="netmi-features">
+<div class="netmi-inner">
+<div class="netmi-income-block">
+<div class="owl-carousel owl-theme">
+<div class="netmi-income-box">
+<div class="netmi-income-box-img">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/home-slide-1-27a1a.png?v=3.10.1726217824" alt="">
+<div class="netmi-income-box-caption">
+    <div class="netmi-income-box-price">
+    <span class="netmi-new"><span class="flaticon-status-1"></span> Active</span>
+    </div>
+    Staking Crypto</div>
+</div>
+<div class="netmi-income-box-info">
+<div class="netmi-income-box-desc">
+<p>Allocate your crypto assets  to receive daily dividends.</p>
+<a href="http://127.0.0.1:8002/register" class="netmi-_link">
+<span class="netmi-b"><i class="flaticon-arr-next-fast"></i></span>
+</a>
+</div>
+</div>
+</div>
+<div class="netmi-income-box">
+<div class="netmi-income-box-img">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/home-slide-27a1a.png?v=3.10.1726217824" alt="">
+<div class="netmi-income-box-caption">
+    <div class="netmi-income-box-price">
+    <span class="netmi-new"><span class="flaticon-status-1"></span> Active</span>
+    </div>
+    Accelerators</div>
+</div>
+<div class="netmi-income-box-info">
+<div class="netmi-income-box-desc">
+<p>Boost your staking returns and get other benefits with special accelerators.</p>
+<a href=" http://127.0.0.1:8002/accelerators" class="netmi-_link">
+<span class="netmi-b"><i class="flaticon-arr-next-fast"></i></span>
+</a>
+</div>
+</div>
+</div>
+<div class="netmi-income-box">
+<div class="netmi-income-box-img">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/home-slide-37a1a.png?v=3.10.1726217824" alt="">
+<div class="netmi-income-box-caption">
+<div class="netmi-income-box-price">
+<span class="netmi-new"><span class="flaticon-status-1"></span> Active</span>
+</div>
+Affiliate Program</div>
+</div>
+<div class="netmi-income-box-info">
+<div class="netmi-income-box-desc">
+<p>Get generous rewards for attracting new partners.</p>
+<a href="partners.html" class="netmi-_link">
+<span class="netmi-b"><i class="flaticon-arr-next-fast"></i></span>
+</a>
+</div>
+</div>
+</div>
+<div class="netmi-income-box">
+<div class="netmi-income-box-img">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/home-slide-47a1a.png?v=3.10.1726217824" alt="">
+<div class="netmi-income-box-caption">
+<div class="netmi-income-box-price">
+<span class="netmi-new"><span class="flaticon-status-1"></span> Active</span>
+</div>  
+Bonus Tasks</div>
+</div>
+<div class="netmi-income-box-info">
+<div class="netmi-income-box-desc">
+<p>Complete paid tasks to receive extra income.</p>
+<a href="tasks.html" class="netmi-_link">
+<span class="netmi-b"><i class="flaticon-arr-next-fast"></i></span>
+</a>
+</div>
+</div>
+</div>
+<div class="netmi-income-box">
+<div class="netmi-income-box-img">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/home-slide-5-17a1a.png?v=3.10.1726217824" alt="">
+<div class="netmi-income-box-caption">
+<div class="netmi-income-box-price" style="background: #0c071a;color: #fff;">
+<span class="netmi-new"><span class="flaticon-status-0"></span> Coming soon</span>
+</div>
+Monthly prize draw</div>
+</div>
+<div class="netmi-income-box-info">
+<div class="netmi-income-box-desc">
+<p>Hit the  Jackpot that is growing on a daily basis.</p>
+<span class="netmi-b" style="background: #8e88a4;"><i class="flaticon-arr-next-fast"></i></span>
+</div>
+</div>
+</div>
+<div class="netmi-income-box">
+<div class="netmi-income-box-img">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/home-slide-6-27a1a.png?v=3.10.1726217824" alt="">
+<div class="netmi-income-box-caption">
+<div class="netmi-income-box-price" style="background: #0c071a;color: #fff;">
+<span class="netmi-new"><span class="flaticon-status-0"></span> Coming soon</span>
+</div>
+Savings Program</div>
+</div>
+<div class="netmi-income-box-info">
+<div class="netmi-income-box-desc">
+<p>Set aside your accruals to get access to VIP and VIP+ Staking plans.</p>
+<span class="netmi-b" style="background: #8e88a4;"><i class="flaticon-arr-next-fast"></i></span>
+</div>
+</div>
+</div>
+<div class="netmi-income-box">
+<div class="netmi-income-box-img">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/home-slide-7-17a1a.png?v=3.10.1726217824" alt="">
+<div class="netmi-income-box-caption">
+<div class="netmi-income-box-price" style="background: #0c071a;color: #fff;">
+<span class="netmi-new"><span class="flaticon-status-0"></span> Coming soon</span>
+</div>   
+Marketplace</div>
+</div>
+<div class="netmi-income-box-info">
+<div class="netmi-income-box-desc">
+<p>Collect, save, and sell Dazzlex tokens at p2p marketplace.</p>
+<span class="netmi-b" style="background: #8e88a4;"><i class="flaticon-arr-next-fast"></i></span>
+</div>
+</div>
+</div>
+<div class="netmi-income-box">
+<div class="netmi-income-box-img">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/home-slide-8-17a1a.png?v=3.10.1726217824" alt="">
+<div class="netmi-income-box-caption">
+<div class="netmi-income-box-price" style="background: #0c071a;color: #fff;">
+<span class="netmi-new"><span class="flaticon-status-0"></span> Coming soon</span></div>
+Loyalty Program</div>
+</div>
+<div class="netmi-income-box-info">
+<div class="netmi-income-box-desc">
+<p>Use more favorable  staking rates with each new re-investment.</p>
+<span class="netmi-b" style="background: #8e88a4;"><i class="flaticon-arr-next-fast"></i></span>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+<div class="gw-header boost-header">
+<div class="container gw-header-body">
+<div class="gw-contest-info">
+<h1 class="gw-header__title">
+Dazzlex token trading</h1>
+<div class="gw-header__txt">
+NEW way to earn extra income by trading Dazzlex. Now you have the opportunity to buy and sell an internal Dazzlex token at any time, right from you user dashboard, and earn at least <b style="color: #fff;">0.5% per day</b> on its price growth. A <b style="color: #fff;">Mini-Pump</b> will be carried out once a month, where the price of the token will rise sharply for a few hours, which will allow Dazzlex holders to realise  quick sizable profits.</div>
+<div class="gw-header__btn">
+	<a href="#" onclick="popup_open('#popup-signup'); return false;" class="netmi-btn netmi-purple" style="background: #141329;">Swap Dazzlex</a>
+   </div>
+</div><div class="gw-contest-col">
+<div class="gw-contest boost-contest"><div class="gw-contest__img">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/home-token-ntm7a1a.png?v=3.10.1726217824" alt="NTM">
+</div>
+<div class="boost-points">
+<div class="boost-points__your">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/buy-ntm.svg?v=3.10.1726217824" alt="NTM">
+<span>Buy Dazzlex</span>
+</div>
+<div class="boost-points__up">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/sell-ntm.svg?v=3.10.1726217824" alt="NTM">
+<span>Sell Dazzlex</span>
+</div>
+</div><div class="boost-pgbar">
+<span style="width: 0%;"></span>
+</div><div class="gw-contest__foot">
+<div class="gw-contest__bank">
+<strong>+0.5%</strong>
+</div>
+<div class="gw-contest__i">
+<p data-tippy-content="Подробнее о системе скидок и бустере вы можете узнать в разделе FAQ на Geekchange.">
+<span>Daily growth rate</span>
+<i class="flaticon-faq-1"></i>
+</p>
+</div>
+</div></div>
+</div></div>
+</div>
+
+<div class="coin-specs__container">
+<div class="netmi-title">
+<h3>Accelerator <span>Features</span></h3>
+<div class="netmi-d">Our unique accelerator system lets you increase your daily returns,  get premium bonuses , and obtain custom Dazzlex tokens.</div>
+</div>
+<div class="coin-specs__head__box">
+<div class="coin-specs__head__box__data__container">
+<div class="coin-specs__head__box__data__head">
+<div class="coin-specs__head__box__data__head__coin">
+<img src="http://127.0.0.1:8002/assets/tpl/images/home/accelerator-check7a1a.svg" alt="" class="coin-specs__head__box__data__head__coin__img">
+<div class="coin-specs__head__box__data__head__coin__name">Dazzlex Price</div>
+</div>
+<div class="coin-specs__head__box__data__head__price">$ 3.7593</div>
+</div>
+<div class="coin-specs__head__box__data__row">
+<div class="coin-specs__head__box__data__col">
+<div class="coin-specs__head__box__data__box ">
+<div class="coin-specs__head__box__data__box__text">Increased Profitability</div>
+<div class="coin-specs__head__box__data__box__data__blue"><img src="http://127.0.0.1:8002/assets/tpl/images/home/accelerator-check7a1a.svg" alt=""></div>
+</div>
+<div class="coin-specs__head__box__data__box ">
+<div class="coin-specs__head__box__data__box__text">Allocation of Dazzlex Tokens</div>
+<div class="coin-specs__head__box__data__box__data__blue"><img src="http://127.0.0.1:8002/assets/tpl/images/home/accelerator-check7a1a.svg" alt=""></div>
+</div>
+<div class="coin-specs__head__box__data__box ">
+<div class="coin-specs__head__box__data__box__text">Cashback to the Main balance</div>
+<div class="coin-specs__head__box__data__box__data__blue"><img src="http://127.0.0.1:8002/assets/tpl/images/home/accelerator-check7a1a.svg" alt=""></div>
+</div>
+</div>
+<div class="coin-specs__head__box__data__col">
+<div class="coin-specs__head__box__data__box ">
+<div class="coin-specs__head__box__data__box__text">Bonus to Premium balance</div>
+<div class="coin-specs__head__box__data__box__data__green"><img src="http://127.0.0.1:8002/assets/tpl/images/home/accelerator-check7a1a.svg" alt=""></div>
+</div>
+<div class="coin-specs__head__box__data__box">
+<div class="coin-specs__head__box__data__box__text ">Stake Amount Bonus (%)</div>
+<div class="coin-specs__head__box__data__box__data__green"><img src="http://127.0.0.1:8002/assets/tpl/images/home/accelerator-check7a1a.svg" alt=""></div>
+</div>
+<div class="coin-specs__head__box__data__box">
+<div class="coin-specs__head__box__data__box__text ">Bonus to the stake amount ($)</div>
+<div class="coin-specs__head__box__data__box__data__green"><img src="http://127.0.0.1:8002/assets/tpl/images/home/accelerator-check7a1a.svg" alt=""></div>
+</div>
+</div>
+</div>
+</div>
+<div class="coin-specs__head__box__chart__container">
+<div class="coin-specs__head__box__chart chart-coin">
+
+<div class="rate-box-new">
+<div class="rate-row-new">
+							<div class="col-12-new col-lg-5-new">
+								<div class="invest__rate-wrap-new">
+									<div class="invest__rate-new">
+										<span>Dazzlex Price</span>
+										<p>$3.7593</p>
+									</div>
+
+									<div class="invest__graph-new">
+										<img src="http://127.0.0.1:8002/assets/tpl/images/icons/graph17a1a.svg" alt="">
+									</div>
+								</div>
+							</div>
+
+							<div class="col-12-new col-lg-5-new offset-lg-1-new">
+								<div class="invest__rate-wrap-new">
+									<div class="invest__rate-new">
+										<span>Change in 24h</span>
+                                        <p class="purple">+0.49%</p>									</div>
+
+									<div class="invest__graph-new">
+										<img src="http://127.0.0.1:8002/assets/tpl/images/icons/graph27a1a.svg" alt="">
+									</div>
+								</div>
+							</div>
+
+							<div class="col-12-new col-lg-5-new">
+								<div class="invest__rate-wrap-new">
+									<div class="invest__rate-new">
+										<span>Change in a week</span>
+                                        <p class="purple">+4.62%</p>									</div>
+
+									<div class="invest__graph-new">
+										<img src="http://127.0.0.1:8002/assets/tpl/images/icons/graph37a1a.svg" alt="">
+									</div>
+								</div>
+							</div>
+
+							<div class="col-12-new col-lg-5-new offset-lg-1-new">
+								<div class="invest__rate-wrap-new">
+									<div class="invest__rate-new">
+										<span>All-time change</span>
+                                        <p class="purple">+564.44%</p>									</div>
+
+									<div class="invest__graph-new">
+										<img src="http://127.0.0.1:8002/assets/tpl/images/icons/graph47a1a.svg" alt="">
+									</div>
+								</div>
+							</div>
+						</div>
+</div>
+
+
+</div>
+</div>
+</div>
+</div>
+<div class="bxhead-info" style="margin: 0px auto; text-align:center">
+<div class="netmi-buttons" style="margin-bottom: 50px;">
+<a href="accelerators.html" class="netmi-btn netmi-purple">Read more</a>
+</div>
+</div>
+
+
+
+<div class="netmi-title">
+<h3>Statistics</h3>
+<div class="netmi-d">Live updated platform performance. Witness our growth and success in real time.</div>
+</div>
+<div class="netmi-features-stat">
+<div class="netmi-item netmi-bloc-1">
+<div class="netmi-icon"><img src="http://127.0.0.1:8002/assets/tpl/images/home/stat-17a1a.svg" alt=""></div>
+<div class="netmi-number"><span>176 762</span></div>
+<div class="netmi-desc">Active Members</div>
+</div>
+<div class="netmi-item netmi-bloc-2">
+<div class="netmi-icon"><img src="http://127.0.0.1:8002/assets/tpl/images/home/stat-27a1a.svg" alt=""></div>
+<div class="netmi-number"><span>14 335 659.71</span>$</div>
+<div class="netmi-desc">Total Investments</div>
+</div>
+<div class="netmi-item netmi-bloc-3">
+<div class="netmi-icon"><img src="http://127.0.0.1:8002/assets/tpl/images/home/stat-37a1a.svg" alt=""></div>
+<div class="netmi-number"><span>2 701 513.31</span>$</div>
+<div class="netmi-desc">Total Profits Paid</div>
+</div>
+<div class="netmi-item netmi-bloc-4">
+<div class="netmi-icon"><img src="http://127.0.0.1:8002/assets/tpl/images/home/stat-47a1a.svg" alt=""></div>
+<div class="netmi-number"><span title="03.09.2023">
+389</span></div>
+<div class="netmi-desc">Days Online</div>
+</div>
+</div>
+</div>
+</div>
+    <div class="netmi-about">
+<div class="netmi-inner">
+<div class="netmi-desc">
+<h3>Dazzlex - <span>your trusted crypto guide</span></h3>
+<div class="netmi-textbox"><p>Netmi is a virtual ecosystem that makes crypto investments available to general public. Everyone can multiply his hard earned capital here. With our professional guidance you don't need to have a degree in finance, special trading or programming skills. It is sufficient to register, choose an investment plan, add funds to your account balance, make some stakes, and the profits will start rolling in.</p></div>
+<div class="netmi-buttons">
+<a href="about.html" class="netmi-btn netmi-purple">Read more</a>
+<a href="how-it-works.html" class="netmi-btn">How it works?</a>
+</div>
+</div>
+<div class="netmi-image">
+<img src="http://127.0.0.1:8002/assets/lp.png" alt="">
+</div>
+</div>
+</div>
+    <div class="netmi-inner">
+<div class="netmi-title">
+<h3>Why <span>choose us?</span></h3>
+<div class="netmi-d">Whether you are a newbie amateur or a seasoned moneymaker Dazzlex is place for you!</div>
+</div>
+<div class="netmi-home-slider-info">
+        <div class="netmi-home-block netmi-hfs-1">
+            <div class="netmi-l"><img src="http://127.0.0.1:8002/assets/tpl/images/home/advantages-17a1a.svg" alt=""></div>
+            <div class="netmi-r">
+            <div><span>1 »</span> 
+                                        <a href="#" onclick="popup_open('#popup-signup'); return false;">Free to join</a>
+                                    </div>
+            Create your account with several clicks.</div>
+            <div class="netmi-cls"></div>
+        </div>
+        <div class="netmi-home-block netmi-hfs-2">
+            <div class="netmi-l"><img src="http://127.0.0.1:8002/assets/tpl/images/home/advantages-27a1a.svg" alt=""></div>
+            <div class="netmi-r"><div><span>2 »</span> Easy to top up</div>
+            Add funds to your account with your favorite crypto.        </div>
+        <div class="netmi-cls"></div>
+        </div>
+        <div class="netmi-home-block netmi-hfs-3">
+            <div class="netmi-l"><img src="http://127.0.0.1:8002/assets/tpl/images/home/advantages-37a1a.svg" alt=""></div>
+            <div class="netmi-r"><div><span>3 »</span> Simple to start</div>
+            Build multiple income streams from staking and rewards.</div>
+            <div class="netmi-cls"></div>
+        </div>
+<div class="netmi-cls"></div>
+</div>
+</div>
+    
+</div>
+</main>
+
+
+@include('layouts.mainsite.footer')
