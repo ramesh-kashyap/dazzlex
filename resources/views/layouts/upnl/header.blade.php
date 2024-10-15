ae697ffc70cef4d2b0ce7db3ee71b9e842f0f2d7
rajkumarkashyap3582 <rajkumarkashyap3582@gmail.com>
2024-10-15 17:17:52 +0530

    dfghj

--- resources/views/layouts/upnl/header.blade.php
+++ resources/views/layouts/upnl/header.blade.php
@@ -290,13 +290,9 @@
                               </svg>
                               
                                Navigation
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 80" width="40" height="40" style="font-size: 24px; display: flex; margin: 0px 9px 0 0; color: #8e88a4;width:17px">
-                                <rect width="100" height="20" fill="currentColor"></rect>
-                                <rect y="30" width="100" height="20" fill="currentColor"></rect>
-                                <rect y="60" width="100" height="20" fill="currentColor"></rect>
-                              </svg>
+                           
                               
-                               Navigation
+                               
                         </span>
                         <ul class="dropdown">
                             <li><a href="{{ route('Index') }}">Home</a></li>
